4f580b7f48afa966b9fecc93ac798392039720bf
Maya <pineaple.aman@gmail.com>
2025-10-18 11:00:57 +0530

    refactoring for /start command

--- commands/_start.js
+++ commands/_start.js
@@ -16,4 +16,22 @@
   group: 
 CMD*/
 
-Bot.sendMessage("ambada thayoli")
+var oldUser = User.GetPropery("oldUser")
+
+if(oldUser){ return } 
+User.setProperty("oldUser", true);
+  var totalUsers = Libs.ResourcesLib.anotherChatRes("total", "global")
+  totalUsers.add(1)
+  var balance = Libs.ResourcesLib.userRes("balance")
+  balance.add(0.5)
+  Bot.sendMessage("welcome bonus 0.5 TRX")
+
+function onAttracted(refUser) {
+   Bot.run({
+    command: "/lvl1",
+    options: { hel: hel }
+  })
+}
+Libs.ReferralLib.track({
+  onAttracted: onAttracted
+})
